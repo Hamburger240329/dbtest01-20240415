f7403f274bc3431123cf69a109a37361f55116a2
JBEDU29 <5514cyj@gmail.com>
2024-04-15 16:11:03 +0900

    insert 문 추가 1. sql = insert 값 추가 2. dbConn.commit() 추가

--- dbtest01.py
+++ dbtest01.py
@@ -14,22 +14,24 @@ dbConn = pymysql.connect(host='localhost', user='root', password='12345', db='sh
 # 파이썬과 mysqlrksmdl connecton 생선 - 커넥션 생성
 
 # 작명 = ~~~
-sql = "SELECT * FROM membertbl"  # DB에 실행할 SQL문 생성
+# sql = "SELECT * FROM membertbl"  # DB에 실행할 SQL문 생성
+sql = "INSERT INTO membertbl VALUES('tiger', '김호랑', '인천 동구')"
 
 cur = dbConn.cursor()
 cur.execute(sql)  # 연결된 DB의 스키마에 지정된 SQL문이 실행됨
 
-records = cur.fetchall()  # sql문에서 실행된 swlect문의 결과를 records로 받음
+# records = cur.fetchall()  # sql문에서 실행된 swlect문의 결과를 records로 받음
 
-print(records)
-print(records[0])  # 특정 레코드(1행)
-print(records[0][1])  # 특정 레코드의 특정 값(ex:이순신)
-
-for member in records:
-    print(member)
+# print(records)
+# print(records[0])  # 특정 레코드(1행)
+# print(records[0][1])  # 특정 레코드의 특정 값(ex:이순신)
+#
+# for member in records:
+#     print(member)
 
 # dbConn 을 열고나서 cur 을 만들었으므로 듣을대도 반대로 cur을 먼저 닫고 dbconn을 닫아야 한다
 # dbconn 의 사용이 종료된 후에는 반드시 닫아줄 것!(close:cur먼저 닫고 dbconn을 닫아야 함)
 cur.close()
+dbConn.commit()  # insert, delete, update 문을 사용한 경우에는 반드시 commint 함수를 호출해야함!!!!!
 dbConn.close()
 
